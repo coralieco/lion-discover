d5934fceb35586eaa9a4c70fadec0eef3c117468
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-04-06 21:21:30 +0200

    adding typed JS

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Découvrez la startup qui vous correspond", "Trouver votre prochain métier", "Rester en contact avec votre startup adorée"],
+    typeSpeed: 65,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
