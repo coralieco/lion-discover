215ae68b4c481abbd5f574ba640f514cc40e7109
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-04-10 11:55:54 +0200

    done

--- config/puma.rb
+++ config/puma.rb
@@ -56,11 +56,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count<br/>
-preload_app!<br/>
-rackup DefaultRackup<br/>
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'<br/>
-on_worker_boot do
-    ActiveRecord::Base.establish_connection
-end
+
